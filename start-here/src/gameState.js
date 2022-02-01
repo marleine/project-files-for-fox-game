1e9b7ecc39891ef3bb102879a1d16ab5beb5e557
Marleine Daoud <marleine@gmail.com>
2022-02-01 13:37:37 -0800

    Start of 'Complete Front-End Project: Build a Game' on Frontend Masters by Brian Holt (until states). https://btholt.github.io/project-fox-game-site/

---
+++ start-here/src/gameState.js
@@ -0,0 +1,15 @@
+const gameState = {
+  current: "INIT",
+  clock: 1,
+  tick() {
+    this.clock++;
+    console.log("clock", this.clock);
+    return this.clock;
+  },
+
+  handleUserAction(icon) {
+    console.log(icon);
+  },
+};
+
+export default gameState;
